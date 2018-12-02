91f551c37315431b367d3269b84654a2bfe165f7
Okan <okanemeni@outlook.com>
2018-12-02 21:57:18 +0100

    Added new migration

--- Migrations/20181202205654_BuildDb.cs
+++ Migrations/20181202205654_BuildDb.cs
@@ -4,7 +4,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace WebApp1.Migrations
 {
-    public partial class RebuildDb : Migration
+    public partial class BuildDb : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {

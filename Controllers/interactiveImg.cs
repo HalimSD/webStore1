152467349ab7dc1dc294177b790fc757e05c5652
HalimSD <a.alhalim94@gmail.com>
2018-12-16 19:27:41 +0100

    interactiveImage not complete

--- Controllers/interactiveImg.cs
+++ Controllers/interactiveImg.cs
@@ -10,28 +10,73 @@ namespace WebApp1.Controllers
 
         private readonly WebshopContext _context;
 
-         public interactiveImg(WebshopContext context)
-            {
-             _context = context;
-            }
+        public interactiveImg(WebshopContext context)
+        {
+            _context = context;
+        }
         [Route("interactiveImg")]
         public IActionResult Index()
         {
-            var soort= _context.Productsoort.ToList().OrderBy(x => x.Naam).Take(2);
-            // .Take(8);
-          
-            return View(soort);
+             var parent1 = (from soort in _context.Productsoort join parent in _context.ParentChild
+                            on soort.Id  equals parent.ParentId
+                            where soort.Id == parent.ParentId && soort.Id != parent.ChildId
+                            select soort).ToList().Distinct();
+                            
+                    //         where p.Parent == null 
+                    //         select p.Child;
+                    //         var x = parent.FirstOrDefault().Children;
+
+                    //          from productsoorten in _context.Productsoort
+                    // join atributen in _context.Attribuutsoort on productsoorten.Id equals atributen.ProductsoortId
+                    // where productsoorten.Naam == productsoortt
+                    // select atributen;
+//             List<ParentChild> parentChild = _context.ParentChild.ToList();
+//             // List<Productwaarde> productwaarde = _context.Productwaarde.ToList();
+//             Productsoort productsoort = new Productsoort();
+//             productsoort.Children = parentChild;
+//             // MultiData finalItem = new MultiData();
+//             // finalItem.parentChild = parentChild;
+//             // finalItem.soort = productsoort;
+//             // finalItem.waarde = productwaarde;
+           
+// var x= productsoort.Children.Select(l=>l.Parent).Where(klaas=>klaas.Id==klaas.Id);
+            return View(parent1);
         }
 
-        [Route("interactiveImg/{id}")]
         public IActionResult child(int id)
         {
-            var soort= _context.Productsoort.Find(id);
-            // ..ToList().OrderBy(x => x.Naam).Take(2);
-            // .Take(8);
-          
-            return View(soort);
+            var child1 = (from soort in _context.Productsoort join parent in _context.ParentChild
+                            on soort.Id  equals parent.ChildId
+                            where id == parent.ParentId
+                            select soort).ToList().Distinct();
+            // from p in _context.ParentChild
+            //                 where p.Child != null 
+            //                 && p.ChildId == id
+            //                 select p.Child;
+            
+            //  var parent = from p in _context.ParentChild
+            //                 where p.Parent != null 
+            //                 && p.ParentId == id
+            //                 select p.Parent;
+            //                 if(child == parent){
+            //                     var l =child.Select(x=>x.Id);
+            //                     l = parent.Select(x=>x.Id);
+            //                     return View("Index");
+            //                 }
+            return View(child1);
         }
+
+         [Route("parent/{id}")]
+        public IActionResult parent(int id)
+        {
+            var parent = from p in _context.ParentChild
+                            where p.Parent != null 
+                            && p.ParentId == id
+                            select p.Parent;
+                            return View(parent.ToList());
+
+        }
+        
     }
 }
 

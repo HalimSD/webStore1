4b22775d3ab289fbf23b261e7cf7b15a2d0fa4dc
Okan <okanemeni@outlook.com>
2019-01-01 22:56:05 +0100

    Improved: navigation hierachy on category/product pages

--- Models/CategoryViewModel.cs
+++ Models/CategoryViewModel.cs
@@ -16,13 +16,15 @@ namespace WebApp1.Models
     {
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
-        public string CategoryPath { get; set; }
+        public List<string[]> CategoryPath { get; set; }
         public PaginationViewModel<Productwaarde> Products { get; set; }
+
         public List<Productsoort> Categories { get; set; }
+
         // Index 0 = attribute name, Index 1 = attribute id
         public List<string[]> Attributes { get; set; }
         public CategoryFilterModel Filters { get; set; }
-        
+
         // The fields below are used to display the correct filter options
         public string[] PriceFilterRange { get; set; }
         public string[] QuantityFilterRange { get; set; }
@@ -35,7 +37,9 @@ namespace WebApp1.Models
         // Model will be expanded as more filter options are added
         public string[] PriceRanges { get; set; }
         public string[] QuantityRanges { get; set; }
+
         public List<AttributeFilter> AttributeFilters { get; set; }
+
         // Used to check if Filter options are empty!
         public bool IsEmpty
         {
@@ -58,6 +62,7 @@ namespace WebApp1.Models
                 {
                     empty = true;
                 }
+
                 return empty && emptyPrice;
             }
         }
@@ -68,7 +73,7 @@ namespace WebApp1.Models
         public int AttributeId { get; set; }
         public string FilterValue { get; set; }
     }
-    
+
     public class CategoryViewModelHelper
     {
         private readonly WebshopContext context;
@@ -79,24 +84,25 @@ namespace WebApp1.Models
             this.context = context;
             this.maxPageSize = maxPageSize;
         }
-        
+
         public CategoryViewModel CreateViewModel(int? categoryId, int pageNumber, CategoryFilterModel filters = null)
         {
             CategoryViewModel viewModel = new CategoryViewModel();
-            PaginationHelper<Productwaarde> productsPage = new PaginationHelper<Productwaarde>(maxPageSize,context.Productwaarde);
-            
+            PaginationHelper<Productwaarde> productsPage =
+                new PaginationHelper<Productwaarde>(maxPageSize, context.Productwaarde);
+
             // If no id was provided, we will display all products
             if (categoryId == null)
             {
                 categoryId = GetRootParentId();
             }
-            
+
             // Get list of categories where we have to retrieve products from
-            List<int> categoryIdList = GetProductCategoryIds((int)categoryId);
-            
+            List<int> categoryIdList = GetProductCategoryIds((int) categoryId);
+
             viewModel.CategoryName =
                 (from ps in context.Productsoort where ps.Id == categoryId select ps.Naam).FirstOrDefault();
-            
+
             // Retrieve all products that belong to one of the categories
             IQueryable<Productwaarde> productsQuery =
             (
@@ -104,33 +110,33 @@ namespace WebApp1.Models
                 where categoryIdList.Contains(pw.ProductsoortId)
                 select pw
             );
-            
+
             viewModel.PriceFilterRange = GetPriceFilterRange(productsQuery);
             viewModel.QuantityFilterRange = GetQuantityFilterRange(productsQuery);
-            
+
             // Apply filters if controller provided us with one
             productsQuery = FilterAttributes(filters, productsQuery);
             productsQuery = FilterPrice(filters, productsQuery);
             productsQuery = FilterQuantity(filters, productsQuery);
-                
+
             viewModel.Filters = filters;
             viewModel.Products = productsPage.GetPageIQueryable(pageNumber, productsQuery);
-            
+
             // Get the attributes of that category
             List<string[]> attributes = new List<string[]>();
             var att =
                 (from atts in context.Attribuutsoort where atts.ProductsoortId == categoryId select atts).ToList();
-            
+
             // And place that data into an 2D array
             // Each element is an array that contains Attribute name and ID
             foreach (var attribute in att)
             {
-                attributes.Add(new []{attribute.Attrbuut, attribute.Id.ToString()});
+                attributes.Add(new[] {attribute.Attrbuut, attribute.Id.ToString()});
             }
 
-            
+
             // Populate the view model with the needed data
-            viewModel.Attributes = attributes;            
+            viewModel.Attributes = attributes;
             viewModel.Categories =
             (
                 from ps in context.Productsoort
@@ -140,9 +146,9 @@ namespace WebApp1.Models
                 select ps
             ).ToList();
 
-            viewModel.CategoryPath = BuildCategoryPath((int)categoryId);
-            viewModel.CategoryId = (int)categoryId;
-            
+            viewModel.CategoryPath = BuildCategoryPath((int) categoryId);
+            viewModel.CategoryId = (int) categoryId;
+
             return viewModel;
         }
 
@@ -156,45 +162,60 @@ namespace WebApp1.Models
             return
             (
                 from r in context.ParentChild
-                where 
-                !(
-                    from c in context.ParentChild 
-                    select c.ChildId
-                ).Contains(r.ParentId)
+                where
+                    !(
+                        from c in context.ParentChild
+                        select c.ChildId
+                    ).Contains(r.ParentId)
                 select r.ParentId
             ).FirstOrDefault();
         }
-        
-        public string BuildCategoryPath(int id)
+
+        public List<string[]> BuildCategoryPath(int id, List<string[]> pathList = null)
         {
             int parentId;
+            if (pathList == null)
+            {
+                pathList = new List<string[]>();
+            }
+
             Productsoort category = (from ps in context.Productsoort where ps.Id == id select ps).FirstOrDefault();
 
+
             IQueryable<int> query =
-            (
                 from pc in context.ParentChild
                 where pc.ChildId == category.Id
-                select pc.ParentId
-            );
+                select pc.ParentId;
 
             if (query.Any())
             {
                 parentId = query.FirstOrDefault();
+                pathList.Add(new[] {category?.Id.ToString(), category?.Naam});
+                BuildCategoryPath(parentId, pathList);
             }
             else
             {
-                string name = (from ps in context.Productsoort where ps.Id == GetRootParentId() select ps.Naam).FirstOrDefault();
-                return name;
+                pathList.Add
+                (
+                    (
+                        from ps in context.Productsoort
+                        where ps.Id == GetRootParentId()
+                        select new[]
+                        {
+                            ps.Id.ToString(),
+                            ps.Naam
+                        }
+                    ).FirstOrDefault()
+                );
             }
 
-            string res = BuildCategoryPath(parentId) + " > " + category.Naam;
-            return res;
+            return pathList;
         }
 
         private string[] GetPriceFilterRange(IQueryable<Productwaarde> query)
         {
             if (!query.Any()) return new string[5];
-            
+
             string[] ranges = new string[5];
             double maxPrice = (from pw in query select pw.Price).Max();
             var maxDiscountPriceQuery =
@@ -210,7 +231,7 @@ namespace WebApp1.Models
             }
 
             double rangeIncr = maxPrice / 5;
-            for (int i = 0; i <= ranges.Length-1; i++)
+            for (int i = 0; i <= ranges.Length - 1; i++)
             {
                 ranges[i] = $"€{rangeIncr * i} - €{rangeIncr * (i + 1)}";
             }
@@ -221,13 +242,13 @@ namespace WebApp1.Models
         private string[] GetQuantityFilterRange(IQueryable<Productwaarde> query)
         {
             if (!query.Any()) return new string[5];
-            
+
             string[] ranges = new string[5];
             int maxQuantity = (from pw in query select pw.Quantity).Max();
             int rangeIncr = maxQuantity / 5;
             for (int i = 0; i <= ranges.Length - 1; i++)
             {
-                ranges[i] = string.Format("{0} stukken - {1} stukken", rangeIncr * i, rangeIncr * (i+1));
+                ranges[i] = string.Format("{0} stukken - {1} stukken", rangeIncr * i, rangeIncr * (i + 1));
             }
 
             return ranges;
@@ -244,7 +265,7 @@ namespace WebApp1.Models
                 select pc.ChildId
             ).ToArray();
 
-                     
+
             foreach (int subId in subCount)
             {
                 var list = GetProductCategoryIds(subId);
@@ -256,7 +277,7 @@ namespace WebApp1.Models
 
             return idArray;
         }
-        
+
         /// <summary>
         /// Converts a price range string (e.g. "€500 - €2999")
         /// and extracts the two numbers from it which will be placed in a double array
@@ -270,7 +291,7 @@ namespace WebApp1.Models
             string[] split = range.Split("-");
             rangeArray[0] = double.Parse(split[0]);
             rangeArray[1] = double.Parse(split[1]);
-            
+
             // Make sure index 0 is min and index 1 is max
             if (rangeArray[0] > rangeArray[1])
             {
@@ -279,7 +300,7 @@ namespace WebApp1.Models
                 rangeArray[0] = min;
                 rangeArray[1] = max;
             }
-            
+
             return rangeArray;
         }
 
@@ -290,7 +311,7 @@ namespace WebApp1.Models
             string[] split = range.Split("-");
             rangeArray[0] = int.Parse(split[0]);
             rangeArray[1] = int.Parse(split[1]);
-            
+
             // Make sure index 0 is min and index 1 is max
             if (rangeArray[0] > rangeArray[1])
             {
@@ -312,7 +333,7 @@ namespace WebApp1.Models
                 filters.PriceRanges = new string[0];
                 return query;
             }
-            
+
             bool filtered = false;
             var filteredQuery = query.Take(0);
             foreach (string item in filters.PriceRanges)
@@ -346,7 +367,7 @@ namespace WebApp1.Models
                     );
                 }
             }
-            
+
             if (filtered) return filteredQuery;
             return query;
         }
@@ -360,7 +381,7 @@ namespace WebApp1.Models
                 filters.QuantityRanges = new string[0];
                 return query;
             }
-            
+
             bool filtered = false;
             var filteredQuery = query.Take(0);
             foreach (string item in filters.QuantityRanges)
@@ -369,7 +390,7 @@ namespace WebApp1.Models
                 {
                     // This bool is used to check if we indeed did filtering 
                     filtered = true;
-                            
+
                     // Get the price range that is used in the filter
                     int[] range = GetQuantityRangeFromString(item);
 
@@ -382,16 +403,16 @@ namespace WebApp1.Models
                     );
                 }
             }
-            
+
             if (filtered) return filteredQuery;
             return query;
         }
-        
+
         private IQueryable<Productwaarde> FilterAttributes(CategoryFilterModel filters, IQueryable<Productwaarde> query)
         {
             if (filters == null) return query;
             if (filters.AttributeFilters == null) return query;
-            
+
             bool filtered = false;
             var filteredQuery = query.Take(0);
             foreach (AttributeFilter item in filters.AttributeFilters)
--- Models/viewProductModel.cs
+++ Models/viewProductModel.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace WebApp1.Models
 {
     public class ViewProductModel
@@ -10,7 +12,7 @@ namespace WebApp1.Models
         public string Image { get; set; }
         public string Description { get; set; }
         public int Quantity { get; set; }
-        public string CategoryPath { get; set; }
+        public List<string[]> CategoryPath { get; set; }
 
     }
     

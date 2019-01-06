fa432abbbbe9fb1cdcf287f44dca877843f3dcd0
DESKTOP-6P67NTL\geord <geordycjd@gmail.com>
2019-01-06 19:10:55 +0100

    Footer altijd onderaan, links in footer werken

--- Controllers/FooterPages.cs
+++ Controllers/FooterPages.cs
@@ -7,13 +7,42 @@ namespace WebApp1.Controllers
 {
     public class FooterPages : Controller
     {
-         public IActionResult Index()
+       
+        public IActionResult Bestellen()
         {
-            return View("Betalen");
+            return View("Bestellen");
         }
         public IActionResult Betalen()
         {
-            return View();
+            return View("Betalen");
+        }
+        public IActionResult Contact()
+        {
+            return View("Contact");
+        }
+        public IActionResult Garantie()
+        {
+            return View("Garantie");
+        }
+        public IActionResult Klantenservice()
+        {
+            return View("Klantenservice");
+        }
+        public IActionResult Leveren()
+        {
+            return View("Leveren");
+        }
+        public IActionResult Over()
+        {
+            return View("Over");
+        }
+        public IActionResult Retourneren()
+        {
+            return View("Retourneren");
+        }
+        public IActionResult Voordelen()
+        {
+            return View("Voordelen");
         }
     }
 }

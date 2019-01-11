04bc3efdd631121a4a6506c8cfbc10da3740e82f
Wanne <0946623@hr.nl>
2019-01-11 21:44:06 +0100

    Productsoort form with at

--- Models/Editproductsoort2.cs
+++ Models/Editproductsoort2.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using WebApp1.Models;
 
 namespace WebApp1.Models
 {
     public class EditproductsoortModel2
     {
-        public string oudeproductsoort{get;set;}
+        
+        public string oldproductsoort{get;set;}
+        [Required]
         public string newproductsoort{get;set;}
+        public IList<Attribuutsoort> atributes{get;set;}
     }
 }

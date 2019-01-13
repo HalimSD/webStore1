b6e48885a249f1e41ec3411c5576f2529788aa74
Okan <okanemeni@outlook.com>
2019-01-13 21:29:54 +0100

    Unlocked userManagement
    Signed-off-by: Okan <okanemeni@outlook.com>

--- Controllers/Admin/UserManagementController.cs
+++ Controllers/Admin/UserManagementController.cs
@@ -29,7 +29,7 @@ namespace ContosoRTM.Controllers
             _roleManager = roleManager;
         }
 
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         [HttpGet]
         public IActionResult Index()
         {
@@ -45,7 +45,7 @@ namespace ContosoRTM.Controllers
             return View(vm);
         }
 
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         [HttpGet]
         public async Task<IActionResult> AddRole(string id)
         {
@@ -59,7 +59,7 @@ namespace ContosoRTM.Controllers
             return View(vm);
         }
 
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         [HttpPost]
         public async Task<IActionResult> AddRole(UserManagementAddRole rvm)
         {

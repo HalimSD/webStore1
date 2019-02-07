274230f2b200bb380fccc151cfe147e3f08859cd
HalimSD <a.alhalim94@gmail.com>
2019-02-07 20:42:19 +0100

    fix order routing

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace WebApp1
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddDbContext<WebshopContext>(opt => opt.UseNpgsql(@"Host=localhost;Database=webShop;Username=postgres;Password=1.Halimsd"));
+            services.AddDbContext<WebshopContext>(opt => opt.UseNpgsql(@"Host=localhost;Database=webShop;Username=postgres;Password="));
 
             services.AddSession(options =>
             {
